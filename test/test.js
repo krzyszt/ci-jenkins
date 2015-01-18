ff3504ce75ffcb46359ba9db499c9cff41331b18
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2015-01-18 11:11:30 +0000

    next changes

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var server = require('../server.js');
 
 describe('GET /', function(){
 	it('respond with Continuous Integration with Jenkins',function(done){
-		supertest(server).get('/').expect('Continuous Integration with Jenkins', done);
+		supertest(server).get('/').expect('Continuous Integration with Jenkins now Jenkins server installed', done);
 	});
 });
